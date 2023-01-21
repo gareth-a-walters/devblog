35dc960f1a3915b0bbf076e3524a24d78047410c
gareth-a-walters <gareth.a.walters@gmail.com>
2023-01-21 01:18:04 +0000

    eslint configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang='en'>
       <head />
       <body>{children}</body>
     </html>
